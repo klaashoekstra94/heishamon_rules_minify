9e65781ea8cb3ad4a80933be05695e9556f2cc1e
Klaas Hoekstra <klaas.hoekstra94@gmail.com>
2024-05-27 21:08:11 +0200

    Add option to only remove comments

--- src/heishamon_rules_minify/cli.py
+++ src/heishamon_rules_minify/cli.py
@@ -9,10 +9,11 @@ def main_cli():
     parser.add_argument("input", help="input rules file")
     parser.add_argument("output", help="output rules file")
     parser.add_argument("-p", "--print", action="store_true", help="print the output on the commandline")
+    parser.add_argument("-c", "--comments_only", action="store_true", help="only remove comments from the input")
     args = parser.parse_args()
 
     with open(args.input, "r") as i:
-        text = Minifier.minify(i.read())
+        text = Minifier.minify(i.read(), args.comments_only)
 
     with open(args.output, "w") as o:
         o.write(text)
